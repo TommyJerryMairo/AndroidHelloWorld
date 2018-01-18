4d0eebf741ebe5233d02c9c9546a9064efd752eb
Tommy Jerry Mairo <tommy.mairo@gmail.com>
2018-01-18 01:17:31 -0800

    Update: Finish implemention of two activities.

--- app/src/main/java/moe/tjm/android_dev/study/helloworld/androidhelloworld/MainActivity.java
+++ app/src/main/java/moe/tjm/android_dev/study/helloworld/androidhelloworld/MainActivity.java
@@ -1,15 +1,30 @@
 package moe.tjm.android_dev.study.helloworld.androidhelloworld;
 
+import android.content.Intent;
+import android.sax.TextElementListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "moe.tjm.android_dev.study.helloworld.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when user taps the Send button */
+    public void sendMessage(View view){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+        editText.setText("");
     }
 
 }
